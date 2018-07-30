b28937b0a06f47f22a468ab6a74d1d933201e3cf
bella52057 <bellabartol04@gmail.com>
2018-07-30 15:11:51 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,7 +1,8 @@
  def invitations(host_name = "Harry Potter", guest_name = "Hermoine", party_name = "Best Halloween Party Ever", date = "October 31", time = "6pm")
   puts "Hi, you've been invited to a party! What is your name?"
   
-  guest_name = get_variable_from_file("./invitation.rb", "guest
+  guest_name = get_variable_from_file("./invitation.rb", "guest_name"
+  
 guest_name = gets.chomp 
 
  puts "Dear #{guest_name},You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than October 30. Sincerely, #{host_name}."
