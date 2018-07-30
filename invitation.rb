be4cb11c2a12c871a188935797a8414244ac9497
bella52057 <bellabartol04@gmail.com>
2018-07-30 15:06:20 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,9 @@
-# Code your prompts here!
+ def invitations(host_name = "Harry Potter", guest_name = "Hermoine", party_name = "Best Halloween Party Ever", date = "October 31", time = "6pm")
+  puts "Hi, you've been invited to a party! What is your name?"
 
-# Try starting out with puts'ing a string.
+guest_name = gets.chomp
+
+ puts "Dear #{guest_name},You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than October 30. Sincerely, #{host_name}."
+
+end
+invitations
